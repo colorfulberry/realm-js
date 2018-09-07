5a14b25155904ad7f83c76147d918ac9b39320ad
Xiaohui Gan <286390860@qq.com>
2018-09-07 13:39:52 +0800

    remove static Server dependency

--- react-native/ios/RealmReact/RealmReact.mm
+++ react-native/ios/RealmReact/RealmReact.mm
@@ -31,17 +31,17 @@
 #import <netdb.h>
 #import <net/if.h>
 
-#if DEBUG
-#import "GCDWebServer.h"
-#import "GCDWebServerDataRequest.h"
-#import "GCDWebServerDataResponse.h"
-#import "GCDWebServerErrorResponse.h"
-#import "rpc.hpp"
-
-#define WEB_SERVER_PORT 8083
-
-using namespace realm::rpc;
-#endif
+//#if DEBUG
+//#import "GCDWebServer.h"
+//#import "GCDWebServerDataRequest.h"
+//#import "GCDWebServerDataResponse.h"
+//#import "GCDWebServerErrorResponse.h"
+//#import "rpc.hpp"
+//
+//#define WEB_SERVER_PORT 8083
+//
+//using namespace realm::rpc;
+//#endif
 
 @interface NSObject ()
 - (instancetype)initWithJSContext:(JSContext *)context;
@@ -85,10 +85,10 @@ extern "C" JSGlobalContextRef RealmReactGetJSGlobalContextForExecutor(id executo
 @implementation RealmReact {
     NSMutableDictionary *_eventHandlers;
 
-#if DEBUG
-    GCDWebServer *_webServer;
-    std::unique_ptr<RPCServer> _rpcServer;
-#endif
+//#if DEBUG
+//    GCDWebServer *_webServer;
+//    std::unique_ptr<RPCServer> _rpcServer;
+//#endif
 }
 
 @synthesize bridge = _bridge;
@@ -120,20 +120,20 @@ RCT_EXPORT_MODULE(Realm)
 }
 
 - (NSDictionary *)constantsToExport {
-#if DEBUG
-#if TARGET_IPHONE_SIMULATOR
-    NSArray *hosts = @[@"localhost"];
-#else
-    NSArray *hosts = [self getIPAddresses];
-#endif
-
-    return @{
-        @"debugHosts": hosts,
-        @"debugPort": @(WEB_SERVER_PORT),
-    };
-#else
+//#if DEBUG
+//#if TARGET_IPHONE_SIMULATOR
+//    NSArray *hosts = @[@"localhost"];
+//#else
+//    NSArray *hosts = [self getIPAddresses];
+//#endif
+//
+//    return @{
+//        @"debugHosts": hosts,
+//        @"debugPort": @(WEB_SERVER_PORT),
+//    };
+//#else
     return @{};
-#endif
+//#endif
 }
 
 - (void)addListenerForEvent:(NSString *)eventName handler:(RealmReactEventHandler)handler {
@@ -155,119 +155,119 @@ RCT_REMAP_METHOD(emit, emitEvent:(NSString *)eventName withObject:(id)object) {
     }
 }
 
-#if DEBUG
-- (NSArray *)getIPAddresses {
-    static const char * const wifiInterface = "en0";
-
-    struct ifaddrs *ifaddrs;
-    if (getifaddrs(&ifaddrs)) {
-        NSLog(@"Failed to get interface addresses: %s", strerror(errno));
-        return @[];
-    }
-
-    NSMutableArray *ipAddresses = [[NSMutableArray alloc] init];
-    char host[INET6_ADDRSTRLEN];
-
-    for (struct ifaddrs *ifaddr = ifaddrs; ifaddr; ifaddr = ifaddr->ifa_next) {
-        if ((ifaddr->ifa_flags & IFF_LOOPBACK) || !(ifaddr->ifa_flags & IFF_UP)) {
-            // Ignore loopbacks and interfaces that aren't up.
-            continue;
-        }
-
-        struct sockaddr *addr = ifaddr->ifa_addr;
-        if (addr->sa_family == AF_INET) {
-            // Ignore link-local ipv4 addresses.
-            in_addr_t sin_addr = ((struct sockaddr_in *)addr)->sin_addr.s_addr;
-            if (IN_LOOPBACK(sin_addr) || IN_LINKLOCAL(sin_addr) || IN_ZERONET(sin_addr)) {
-                continue;
-            }
-        }
-        else if (addr->sa_family == AF_INET6) {
-            // Ignore link-local ipv6 addresses.
-            struct in6_addr *sin6_addr = &((struct sockaddr_in6 *)addr)->sin6_addr;
-            if (IN6_IS_ADDR_LOOPBACK(sin6_addr) || IN6_IS_ADDR_LINKLOCAL(sin6_addr) || IN6_IS_ADDR_UNSPECIFIED(sin6_addr)) {
-                continue;
-            }
-        }
-        else {
-            // Ignore addresses that are not ipv4 or ipv6.
-            continue;
-        }
-
-        if (strcmp(ifaddr->ifa_name, wifiInterface)) {
-            // Ignore non-wifi addresses.
-            continue;
-        }
-        if (int error = getnameinfo(addr, addr->sa_len, host, sizeof(host), NULL, 0, NI_NUMERICHOST)) {
-            NSLog(@"Couldn't resolve host name for address: %s", gai_strerror(error));
-            continue;
-        }
-
-        [ipAddresses addObject:@(host)];
-    }
-    
-    freeifaddrs(ifaddrs);
-    return [ipAddresses copy];
-}
-
-- (void)startRPC {
-    [GCDWebServer setLogLevel:3];
-    _webServer = [[GCDWebServer alloc] init];
-    _rpcServer = std::make_unique<RPCServer>();
-    __weak __typeof__(self) weakSelf = self;
-
-    // Add a handler to respond to POST requests on any URL
-    [_webServer addDefaultHandlerForMethod:@"POST"
-                              requestClass:[GCDWebServerDataRequest class]
-                              processBlock:^GCDWebServerResponse *(GCDWebServerRequest* request) {
-        __typeof__(self) self = weakSelf;
-        RPCServer *rpcServer = self ? self->_rpcServer.get() : nullptr;
-        GCDWebServerResponse *response;
-
-        try {
-            NSData *responseData;
-
-            if (rpcServer) {
-                json args = json::parse([[(GCDWebServerDataRequest *)request text] UTF8String]);
-                std::string responseText = rpcServer->perform_request(request.path.UTF8String, args).dump();
-
-                responseData = [NSData dataWithBytes:responseText.c_str() length:responseText.length()];
-            }
-            else {
-                // we have been deallocated
-                responseData = [NSData data];
-            }
-
-            response = [[GCDWebServerDataResponse alloc] initWithData:responseData contentType:@"application/json"];
-        }
-        catch(std::exception &ex) {
-            NSLog(@"Invalid RPC request - %@", [(GCDWebServerDataRequest *)request text]);
-            response = [GCDWebServerErrorResponse responseWithClientError:kGCDWebServerHTTPStatusCode_UnprocessableEntity
-                                                          underlyingError:nil
-                                                                  message:@"Invalid RPC request"];
-        }
-
-        [response setValue:@"http://localhost:8081" forAdditionalHeader:@"Access-Control-Allow-Origin"];
-        return response;
-    }];
-
-    [_webServer startWithPort:WEB_SERVER_PORT bonjourName:nil];
-    return;
-}
-
-- (void)shutdownRPC {
-    [_webServer stop];
-    [_webServer removeAllHandlers];
-    _webServer = nil;
-    _rpcServer.reset();
-}
-#endif
+//#if DEBUG
+//- (NSArray *)getIPAddresses {
+//    static const char * const wifiInterface = "en0";
+//
+//    struct ifaddrs *ifaddrs;
+//    if (getifaddrs(&ifaddrs)) {
+//        NSLog(@"Failed to get interface addresses: %s", strerror(errno));
+//        return @[];
+//    }
+//
+//    NSMutableArray *ipAddresses = [[NSMutableArray alloc] init];
+//    char host[INET6_ADDRSTRLEN];
+//
+//    for (struct ifaddrs *ifaddr = ifaddrs; ifaddr; ifaddr = ifaddr->ifa_next) {
+//        if ((ifaddr->ifa_flags & IFF_LOOPBACK) || !(ifaddr->ifa_flags & IFF_UP)) {
+//            // Ignore loopbacks and interfaces that aren't up.
+//            continue;
+//        }
+//
+//        struct sockaddr *addr = ifaddr->ifa_addr;
+//        if (addr->sa_family == AF_INET) {
+//            // Ignore link-local ipv4 addresses.
+//            in_addr_t sin_addr = ((struct sockaddr_in *)addr)->sin_addr.s_addr;
+//            if (IN_LOOPBACK(sin_addr) || IN_LINKLOCAL(sin_addr) || IN_ZERONET(sin_addr)) {
+//                continue;
+//            }
+//        }
+//        else if (addr->sa_family == AF_INET6) {
+//            // Ignore link-local ipv6 addresses.
+//            struct in6_addr *sin6_addr = &((struct sockaddr_in6 *)addr)->sin6_addr;
+//            if (IN6_IS_ADDR_LOOPBACK(sin6_addr) || IN6_IS_ADDR_LINKLOCAL(sin6_addr) || IN6_IS_ADDR_UNSPECIFIED(sin6_addr)) {
+//                continue;
+//            }
+//        }
+//        else {
+//            // Ignore addresses that are not ipv4 or ipv6.
+//            continue;
+//        }
+//
+//        if (strcmp(ifaddr->ifa_name, wifiInterface)) {
+//            // Ignore non-wifi addresses.
+//            continue;
+//        }
+//        if (int error = getnameinfo(addr, addr->sa_len, host, sizeof(host), NULL, 0, NI_NUMERICHOST)) {
+//            NSLog(@"Couldn't resolve host name for address: %s", gai_strerror(error));
+//            continue;
+//        }
+//
+//        [ipAddresses addObject:@(host)];
+//    }
+//
+//    freeifaddrs(ifaddrs);
+//    return [ipAddresses copy];
+//}
+//
+//- (void)startRPC {
+//    [GCDWebServer setLogLevel:3];
+//    _webServer = [[GCDWebServer alloc] init];
+//    _rpcServer = std::make_unique<RPCServer>();
+//    __weak __typeof__(self) weakSelf = self;
+//
+//    // Add a handler to respond to POST requests on any URL
+//    [_webServer addDefaultHandlerForMethod:@"POST"
+//                              requestClass:[GCDWebServerDataRequest class]
+//                              processBlock:^GCDWebServerResponse *(GCDWebServerRequest* request) {
+//        __typeof__(self) self = weakSelf;
+//        RPCServer *rpcServer = self ? self->_rpcServer.get() : nullptr;
+//        GCDWebServerResponse *response;
+//
+//        try {
+//            NSData *responseData;
+//
+//            if (rpcServer) {
+//                json args = json::parse([[(GCDWebServerDataRequest *)request text] UTF8String]);
+//                std::string responseText = rpcServer->perform_request(request.path.UTF8String, args).dump();
+//
+//                responseData = [NSData dataWithBytes:responseText.c_str() length:responseText.length()];
+//            }
+//            else {
+//                // we have been deallocated
+//                responseData = [NSData data];
+//            }
+//
+//            response = [[GCDWebServerDataResponse alloc] initWithData:responseData contentType:@"application/json"];
+//        }
+//        catch(std::exception &ex) {
+//            NSLog(@"Invalid RPC request - %@", [(GCDWebServerDataRequest *)request text]);
+//            response = [GCDWebServerErrorResponse responseWithClientError:kGCDWebServerHTTPStatusCode_UnprocessableEntity
+//                                                          underlyingError:nil
+//                                                                  message:@"Invalid RPC request"];
+//        }
+//
+//        [response setValue:@"http://localhost:8081" forAdditionalHeader:@"Access-Control-Allow-Origin"];
+//        return response;
+//    }];
+//
+//    [_webServer startWithPort:WEB_SERVER_PORT bonjourName:nil];
+//    return;
+//}
+//
+//- (void)shutdownRPC {
+//    [_webServer stop];
+//    [_webServer removeAllHandlers];
+//    _webServer = nil;
+//    _rpcServer.reset();
+//}
+//#endif
 
 - (void)invalidate {
-#if DEBUG
-    // shutdown rpc if in chrome debug mode
-    [self shutdownRPC];
-#endif
+//#if DEBUG
+//    // shutdown rpc if in chrome debug mode
+//    [self shutdownRPC];
+//#endif
 }
 
 - (void)dealloc {
@@ -298,11 +298,11 @@ void _initializeOnJSThread(JSContextRefExtractor jsContextExtractor) {
     s_currentModule = self;
 
     if (objc_lookUpClass("RCTWebSocketExecutor") && [bridge executorClass] == objc_lookUpClass("RCTWebSocketExecutor")) {
-#if DEBUG
-        [self startRPC];
-#else
-        @throw [NSException exceptionWithName:@"Invalid Executor" reason:@"Chrome debug mode not supported in Release builds" userInfo:nil];
-#endif
+//#if DEBUG
+//        [self startRPC];
+//#else
+//        @throw [NSException exceptionWithName:@"Invalid Executor" reason:@"Chrome debug mode not supported in Release builds" userInfo:nil];
+//#endif
     } else if ([bridge isKindOfClass:objc_lookUpClass("RCTCxxBridge")]) {
         // probe for the new C++ bridge in React Native 0.45+
 
